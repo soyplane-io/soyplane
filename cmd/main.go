e97d89db0f0e2a1587fdc753469c8cff04b35769
Othmane El Warrak <othmanelwarrak@gmail.com>
2025-04-06 18:11:07 +0000

    scaffold tofu provider

--- cmd/main.go
+++ cmd/main.go
@@ -36,6 +36,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	opentofuv1alpha1 "github.com/soyplane-io/soyplane/api/opentofu/v1alpha1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -47,6 +49,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(opentofuv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
